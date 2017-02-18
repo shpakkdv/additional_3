746716ae579f1d78d89aadc319f960c88d2f5622
shpakkdv <shpak_kdv@mail.ru>
2017-02-18 22:26:04 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getBase(base) {
-  // your solution
-}
+    var syst = 2, temp, string = '', remain;
+    if (base.length > 14) {
+      return new Array(base.length).join('9');
+    }
+    while (syst <= +base) {
+      temp = +base;
+      
+      do {
+        remain = temp % syst;
+        if (remain !== 1) {
+          string = '';
+          break;
+        }
+        string = remain + string;
+        temp = Math.floor(temp / syst);
+      } while (temp !== 0);
+
+      if ( string !== '' ) {
+        return syst+'';
+      }
+      syst++;
+    }
+};
